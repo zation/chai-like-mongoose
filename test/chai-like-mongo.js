9f13da076053ea223195a341601ef6857807eb0b
Liu Yang <zation1@gmail.com>
2015-06-04 12:07:14 +0800

    change mongodb to mongoose

--- test/chai-like-mongo.js
+++ test/chai-like-mongo.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var like = require('chai-like');
-var mongodb = require('mongodb');
+var mongoose = require('mongoose');
 
 like.extend(require('../index'));
 chai.should();
@@ -8,7 +8,7 @@ chai.use(like);
 
 describe('chai-like-mongo', function() {
   it('should compare ObjectId', function() {
-    var objectId = mongodb.ObjectID('123456789012345678901234');
+    var objectId = mongoose.Types.ObjectId('123456789012345678901234');
     objectId.should.like('123456789012345678901234');
     objectId.should.not.like('123456789012345678901235');
   });
